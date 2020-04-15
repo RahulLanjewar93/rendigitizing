97b6e776be5472c2d1d8b1d21004bde04c2a9567
Rishav <coder.rishav@gmail.com>
2020-04-15 19:23:24 +0530

    Pagination and Table show added to Vector Art (vectorart.php) DateTime : 7.24PM 15-04-2020

--- user/newprofile/vectorart.php
+++ user/newprofile/vectorart.php
@@ -1,3 +1,36 @@
+<?php
+session_start();
+require_once "../../db/connection/conn.php";
+
+if(isset($_SESSION['USER']))
+{
+    //Pagination
+    if(isset($_GET['page']))
+    {
+        $page = $_GET['page'];
+    }
+    else
+    {
+        $page = 1;
+    }
+
+    $num_per_page = 01;
+    $start_from = ($page-1)*01;
+    $fetchva = "SELECT * FROM tbl_order WHERE category = 'Emboridery Vector' LIMIT $start_from, $num_per_page";
+    $fetchvaFire = mysqli_query($conn, $fetchva);
+
+    if(mysqli_num_rows($fetchvaFire)>1)
+    {
+        //
+    }
+    else{
+        $eiOrderEmptyMessage = "No orders availabe right now";
+    }
+}
+else{
+    header("location:http://localhost/RenDigitizingUpdated/user/authentication/login.php?nosession=true");
+}
+?>
 <html>
 
 <head>
@@ -110,135 +143,108 @@
                 <h1 class="profile-text-area">Vector Art</h1>
               </div>
               <div class="col-md-6">
-                <nav aria-label="Page navigation example" class="my-2">
-                  <ul class="pagination justify-content-end" id="Pagination">
-                    <li class="page-item">
-                      <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                      <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
+                  <nav aria-label="Page navigation example" class="my-2">
+                      <?php
+                      $getVARecords = "SELECT * FROM tbl_order WHERE category = 'Emboridery Vector'";
+                      $getVARecordsFire = mysqli_query($conn, $getVARecords);
+                      $total_records = mysqli_num_rows($getVARecordsFire);
+
+                      $total_pages = ceil($total_records/$num_per_page);
+
+
+                      ?>
+
+                      <ul class="pagination justify-content-end" id="Pagination">
+                          <?php
+                          if($page>1)
+                          { ?>
+                              <li class="page-item">
+                                  <a class="page-link" href="vectorart.php.php?page=<?php echo ($page-1) ?>" aria-label="Previous">
+                                      <span aria-hidden="true">&laquo;</span>
+                                      <span class="sr-only">Previous</span>
+                                  </a>
+                              </li>
+                          <?php } ?>
+                          <?php
+                          for($i=1; $i<$total_pages; $i++)
+                          {?>
+                              <li class="page-item"><a class="page-link" href="vectorart.php?page=<?php echo $i ?>"><?php echo $i ?></a></li>
+                          <?php } ?>
+                          <?php
+                          if($i>$page)
+                          {
+                              ?>
+                              <li class="page-item">
+                                  <a class="page-link" href="vectorart.php?page=<?php echo ($page+1)?>" aria-label="Next">
+                                      <span aria-hidden="true">&raquo;</span>
+                                      <span class="sr-only">Next</span>
+                                  </a>
+                              </li>
+                          <?php } ?>
+                      </ul>
+                  </nav>
               </div>
             </div>
             <table class="table table-striped table-items">
               <thead>
                 <tr>
                   <th>Image</th>
-                  <th>Name</th>
-                  <th>Quantity</th>
+                  <th>Design Name</th>
                   <th>Price</th>
                   <th>Status</th>
-                  <th>Estimated Delivery</th>
                   <th>Action</th>
                 </tr>
               </thead>
               <tbody>
+              <?php
+                    while ($rows = mysqli_fetch_array($fetchvaFire)){
+              ?>
                 <tr>
-                  <td> <img class="table-image py-2" src="../../styleassets/images/items/item1.jpg" alt=""> </td>
-                  <td>Embroidery</td>
-                  <td> 1 </td>
-                  <td> 250 </td>
-                  <td> In Transit </td>
-                  <td> 26 Jan </td>
+                  <td> <img class="table-image py-2" src="Uploads/Vector/DesignImages/<?php echo $rows['emboridery_vector_design_image']?>" alt=""> </td>
+                  <td><?php echo $rows['design_name']?></td>
+                  <td> <?php echo $rows['price']?> </td>
+                  <td> <?php echo $rows['order_flag']?> </td>
                   <td>
                     <div class="row order-button-group d-block">
                       <div class="col-md-12">
-                        <button class="btn order-btn-1 d-block py-2">View</button>
-                        <button class="btn order-btn-2 d-block py-2">Edit</button></div>
+                          <?php $OrderId = mysqli_real_escape_string($conn, $rows['order_id']); ?>
+                        <a href="details.php?orderid=<?php echo $OrderId?>" class="btn order-btn-1 d-block py-2">View</a>
+                        <a href="edit.php?orderid=<?php echo $OrderId?>" class="btn order-btn-2 d-block py-2">Edit</a></div>
                     </div>
                   </td>
                 </tr>
-                <tr>
-                  <td> <img class="table-image py-2" src="../../styleassets/images/items/item1.jpg" alt=""> </td>
-                  <td> Embroidery </td>
-                  <td> 1 </td>
-                  <td> 250 </td>
-                  <td> In Transit </td>
-                  <td> 26 Jan </td>
-                  <td>
-                    <div class="row order-button-group d-block">
-                      <div class="col-md-12">
-                        <button class="btn order-btn-1 d-block py-2">View</button>
-                        <button class="btn order-btn-2 d-block py-2">Edit</button></div>
-                    </div>
-                  </td>
-                </tr>
-                <tr>
-                  <td> <img class="table-image py-2" src="../../styleassets/images/items/item1.jpg" alt=""> </td>
-                  <td> Embroidery </td>
-                  <td> 1 </td>
-                  <td> 250 </td>
-                  <td> In Transit </td>
-                  <td> 26 Jan </td>
-                  <td>
-                    <div class="row order-button-group d-block">
-                      <div class="col-md-12">
-                        <button class="btn order-btn-1 d-block py-2">View</button>
-                        <button class="btn order-btn-2 d-block py-2">Edit</button></div>
-                    </div>
-                  </td>
-                </tr>
-                <tr>
-                  <td> <img class="table-image py-2" src="../../styleassets/images/items/item1.jpg" alt=""> </td>
-                  <td> Embroidery </td>
-                  <td> 1 </td>
-                  <td> 250 </td>
-                  <td> In Transit </td>
-                  <td> 26 Jan </td>
-                  <td>
-                    <div class="row order-button-group d-block">
-                      <div class="col-md-12">
-                        <button class="btn order-btn-1 d-block py-2">View</button>
-                        <button class="btn order-btn-2 d-block py-2">Edit</button></div>
-                    </div>
-                  </td>
-                </tr>
-                <tr>
-                  <td> <img class="table-image py-2" src="../../styleassets/images/items/item1.jpg" alt=""> </td>
-                  <td> Embroidery </td>
-                  <td> 1 </td>
-                  <td> 250 </td>
-                  <td> In Transit </td>
-                  <td> 26 Jan </td>
-                  <td>
-                    <div class="row order-button-group d-block">
-                      <div class="col-md-12">
-                        <button class="btn order-btn-1 d-block py-2">View</button>
-                        <button class="btn order-btn-2 d-block py-2">Edit</button></div>
-                    </div>
-                  </td>
-                </tr>
+              <?php } ?>
               </tbody>
             </table>
             <nav aria-label="Page navigation example" class="my-2">
-                  <ul class="pagination justify-content-end" id="Pagination">
-                    <li class="page-item">
-                      <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                      <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                      </a>
-                    </li>
-                  </ul>
+                <ul class="pagination justify-content-end" id="Pagination">
+                    <?php
+                    if($page>1)
+                    { ?>
+                        <li class="page-item">
+                            <a class="page-link" href="vectorart.php.php?page=<?php echo ($page-1) ?>" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                        </li>
+                    <?php } ?>
+                    <?php
+                    for($i=1; $i<$total_pages; $i++)
+                    {?>
+                        <li class="page-item"><a class="page-link" href="vectorart.php?page=<?php echo $i ?>"><?php echo $i ?></a></li>
+                    <?php } ?>
+                    <?php
+                    if($i>$page)
+                    {
+                        ?>
+                        <li class="page-item">
+                            <a class="page-link" href="vectorart.php?page=<?php echo ($page+1)?>" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </li>
+                    <?php } ?>
+                </ul>
                 </nav>
           </div>
         </div>
